96855d2d05e45d8cc1593fe30968b65ee5670b49
Frederico Rufino <123844821+fredcode-br@users.noreply.github.com>
2023-10-31 21:03:43 -0300

    Update Footer

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@
 const Footer = () => {
     return (
         <footer className="flex justify-center absolute bottom-5 w-full ">
-            <a href="fredcode.com.br" >fredcode.com.br</a>
+            <a href="https://fredcode.com.br/" >fredcode.com.br</a>
         </footer>
     )
 }
